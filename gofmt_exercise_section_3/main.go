4364e5264b661ce4824b9e5e64cdc08c2c35d98f
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 00:54:25 -0500

    After gofmt exercise from section 3

--- gofmt_exercise_section_3/main.go
+++ gofmt_exercise_section_3/main.go
@@ -1,9 +1,13 @@
 package main
+
 import "fmt"
-func main(){	SayHello()
+
+func main() {
+	SayHello()
 }
 func SayHello() {
 	fmt.Println("Hello package v1.0.0!")
 
-	var x int=10
-	_=x}
+	var x int = 10
+	_ = x
+}
