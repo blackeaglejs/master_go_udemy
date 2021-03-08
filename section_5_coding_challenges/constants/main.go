e77883d04091b51a0d684b2d4165c10f30a9a492
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 23:36:54 -0500

    Section 5, Constant Exercises, Coding Exercise #5

--- section_5_coding_challenges/constants/main.go
+++ section_5_coding_challenges/constants/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	const (
@@ -15,10 +17,26 @@ func main() {
 	const daysYear = 365
 	fmt.Printf("Number of seconds in a year is %v\n", secPerDay*daysYear)
 
+	// Excercise 4 - fix below code.
 	const x int = 10
 
 	// declaring a const of type slice int([]int) The fix is to comment this out. there isn't such a thing as an
 	// array or slice literal which means no array or slice constants.
 	// const m = []int{1: 3, 4: 5, 6: 8}
 	// _ = m
+
+	// Exercise 5 - fix below code.
+
+	const a int = 7
+	const b float64 = 5.6
+	// The const c setting can be fixed by fixing mismatched types.
+	const c = float64(a) * b
+
+	// This code can be fixed by commenting it out OR setting xc explicitly to 8, as you can't set a constant
+	// with a variable.
+	// x = 8
+	const xc int = 8
+
+	// This code has to be commented out because you can't use a runtime function to set a constant.
+	// const noIPv6 = math.Pow(2, 128)
 }
