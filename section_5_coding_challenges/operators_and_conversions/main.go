9eeaf6fe541f8b6d6dd4c78c28020292b0cc176a
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-08 00:22:50 -0500

    Section 5, operator/conversion Exercises, Coding Exercise #5

--- section_5_coding_challenges/operators_and_conversions/main.go
+++ section_5_coding_challenges/operators_and_conversions/main.go
@@ -10,6 +10,7 @@ func main() {
 	exerciseTwo()
 	exerciseThree()
 	exerciseFour()
+	exerciseFive()
 }
 
 func exerciseOne() {
@@ -61,4 +62,18 @@ func exerciseFour() {
 
 	timeToEarth := sunEarthDistance / speedInkms
 	fmt.Printf("Time from Sun to Earth in Seconds Is %f seconds OR %f minutes\n", timeToEarth, (timeToEarth / 60))
+}
+
+func exerciseFive() {
+	x, y := 0.1, 5
+	var z float64 // z is initialized as zero
+
+	// Write the correct logical operator (&&, || , !)
+	// inside the expression so that result1 will be false and result2 will be true.
+
+	result1 := x < z && int(x) != int(z)
+	fmt.Println(result1)
+
+	result2 := y == 1*5 || int(z) == 0
+	fmt.Println(result2)
 }
