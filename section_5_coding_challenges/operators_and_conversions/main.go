4a10c40935925bb9d7bf26480016f43cc5282a63
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-08 00:13:47 -0500

    Section 5, operator/conversion Exercises, Coding Exercise #2

--- section_5_coding_challenges/operators_and_conversions/main.go
+++ section_5_coding_challenges/operators_and_conversions/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	exerciseOne()
+	exerciseTwo()
 }
 
 func exerciseOne() {
@@ -13,4 +17,25 @@ func exerciseOne() {
 	iFloat := float64(i)
 	fInt := int(f)
 	fmt.Printf("iFloat is value %f, type %T; fInt is value %d, type %T\n", iFloat, iFloat, fInt, fInt)
+}
+
+func exerciseTwo() {
+	var i = 3
+	var f = 3.2
+	var s1, s2 = "3.14", "5"
+
+	// Convert i to string
+	iString := strconv.Itoa(i)
+	// convert s2 to int
+	s2Int, _ := strconv.Atoi(s2)
+	// Convert f to string
+	fString := strconv.FormatFloat(f, 'f', -1, 64)
+	// Convert s2 fo float64
+	s1Float64, _ := strconv.ParseFloat(s1, 64)
+
+	// Print out the value and type of each.
+	fmt.Printf("iString value is %s, type is %T\n", iString, iString)
+	fmt.Printf("s2Int value is %d, type is %T\n", s2Int, s2Int)
+	fmt.Printf("fString value is %s, type is %T\n", fString, fString)
+	fmt.Printf("s1Float64 value is %f, type is %T\n", s1Float64, s1Float64)
 }
