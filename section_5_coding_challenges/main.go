e90065fb48a030b4961dc24f4e4219c99d1dfb69
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 23:11:45 -0500

    Section 5, coding exercise #2

--- section_5_coding_challenges/main.go
+++ section_5_coding_challenges/main.go
@@ -1,22 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a int
-	var b float64
-	var c bool
-	var d string
+	var (
+		a int
+		b float64
+		c bool
+		d string
+	)
 
-	x := 20
-	y := 15.5
-	z := "Gopher"
+	x, y, z := 20, 15.5, "Gopher"
 
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
+	// fmt.Println(x)
+	// fmt.Println(y)
+	// fmt.Println(z)
+	_, _, _, _, _, _, _ = a, b, c, d, x, y, z
+
 }
