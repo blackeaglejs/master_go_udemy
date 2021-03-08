4f6d861c59baa81925710006214a6607ffa0ff43
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 23:14:26 -0500

    Section 5, coding exercise #3

--- section_5_coding_challenges/main.go
+++ section_5_coding_challenges/main.go
@@ -1,22 +1,27 @@
 package main
 
 func main() {
-	var (
-		a int
-		b float64
-		c bool
-		d string
-	)
+	// var (
+	// 	a int
+	// 	b float64
+	// 	c bool
+	// 	d string
+	// )
 
-	x, y, z := 20, 15.5, "Gopher"
+	// x, y, z := 20, 15.5, "Gopher"
 
-	// fmt.Println(a)
-	// fmt.Println(b)
-	// fmt.Println(c)
-	// fmt.Println(d)
-	// fmt.Println(x)
-	// fmt.Println(y)
-	// fmt.Println(z)
-	_, _, _, _, _, _, _ = a, b, c, d, x, y, z
+	// // fmt.Println(a)
+	// // fmt.Println(b)
+	// // fmt.Println(c)
+	// // fmt.Println(d)
+	// // fmt.Println(x)
+	// // fmt.Println(y)
+	// // fmt.Println(z)
+	// _, _, _, _, _, _, _ = a, b, c, d, x, y, z
+
+	var a float64 = 7.1
+	x, y := true, 3.7
+	a, x = 5.5, false
+	_, _, _ = a, x, y
 
 }
