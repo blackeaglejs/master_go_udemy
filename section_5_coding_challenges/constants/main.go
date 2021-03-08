ec5d35dc4117c143b71143cf290f14b88718fa3b
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 23:20:30 -0500

    Section 5, Constant Exercises, Coding Exercise #2

--- section_5_coding_challenges/constants/main.go
+++ section_5_coding_challenges/constants/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	const daysWeek = 7
-	const lightSpeed = 299792458
-	const pi = 3.14159
+	const (
+		daysWeek   = 7
+		lightSpeed = 299792458
+		pi         = 3.14159
+	)
 
 	fmt.Printf("Days In Week: %v, Speed of Light (m/s): %v, Value of Pi: %v\n", daysWeek, lightSpeed, pi)
 }
