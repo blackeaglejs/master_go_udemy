f9e6c5befe741e60a81a7e6d21da5fdeed2baf14
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-07 23:08:20 -0500

    Section 5, Coding Exercise #1

---
+++ section_5_coding_challenges/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+
+	x := 20
+	y := 15.5
+	z := "Gopher"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
