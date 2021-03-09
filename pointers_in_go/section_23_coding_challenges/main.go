00e11abb93bf2d314710a70e752e337841044de0
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-09 09:39:37 -0500

    more comments on pointer exercises

--- pointers_in_go/section_23_coding_challenges/main.go
+++ pointers_in_go/section_23_coding_challenges/main.go
@@ -15,6 +15,7 @@ func main() {
 	fmt.Printf("x is %v, y is %v\n", x, y)
 }
 
+// ExerciseOne has me declare a pointer to a variable and then show the address of the pointer, its vaLue, and the original variable's address.
 func ExerciseOne() {
 	x := 10.10
 	fmt.Printf("Address of x: %v\n", &x)
@@ -23,6 +24,7 @@ func ExerciseOne() {
 	fmt.Printf("ptr address is %v, value of x through pointer is %v\n", &ptr, *ptr)
 }
 
+// ExerciseTwo has me divide two variables through their pointers and then store that value in a new variable.
 func ExerciseTwo() {
 	x, y := 10, 2
 	ptrX, ptrY := &x, &y
@@ -30,7 +32,7 @@ func ExerciseTwo() {
 	fmt.Println("Value of z is", z)
 }
 
-// ExerciseThree() Swaps the values of the variables using pointers.
+// ExerciseThree Swaps the values of the variables using pointers.
 func ExerciseThree(x *float64, y *float64) {
 	*x = *y
 	*y = *x
