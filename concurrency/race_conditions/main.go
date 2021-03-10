29f92d799a8b76fabb6355e3eab2a1eacd4ae7ff
Zoheb Nensey <zoheb.nensey@digi.com>
2021-03-09 23:54:10 -0500

    fixing race conditions with mutexes

--- concurrency/race_conditions/main.go
+++ concurrency/race_conditions/main.go
@@ -14,17 +14,29 @@ func main() {
 
 	var n int = 0
 
-	// The below series of functions is a race condition. The reason this is the case is because both functions are operating on the same data spce.
+	// Fixing a race condition using a mutex.
+	//1.
+	var m sync.Mutex
+
+	// The below series of functions is a (fixed) race condition. The reason this is the case is because both functions are operating on the same data spce.
 	// This is not goood because it means the final result of the variable n will be unpredictable and dependent on the order execution occurs.
+	// You can use race detector from golang to find race conditions.
+	// It is called by adding the -race condition to your go run command.
 	for i := 0; i < gr; i++ {
 		go func() {
 			time.Sleep(time.Second / 10)
+			//2.
+			m.Lock() // locks this memory space temporarily so that only one goroutine can access a memory space at a time.
+			defer m.Unlock()
 			n++
 			wg.Done()
 		}()
 
 		go func() {
 			time.Sleep(time.Second / 10)
+			//2.
+			m.Lock()
+			defer m.Unlock()
 			n--
 			wg.Done()
 		}()
